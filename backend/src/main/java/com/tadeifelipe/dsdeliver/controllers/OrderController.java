4f2d23592af2f18072bb24f4e0f8809ad0b255d4
felipe_santostadei@hotmail.com <felipe_santostadei@hotmail.com>
2021-01-08 16:18:10 -0300

    GET \products and GET \orders finalized

---
+++ backend/src/main/java/com/tadeifelipe/dsdeliver/controllers/OrderController.java
@@ -0,0 +1,26 @@
+package com.tadeifelipe.dsdeliver.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tadeifelipe.dsdeliver.dto.OrderDTO;
+import com.tadeifelipe.dsdeliver.services.OrderService;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderController {
+	
+	@Autowired
+	private OrderService service;
+	
+	@GetMapping
+	public ResponseEntity<List<OrderDTO>> findAll(){
+		List<OrderDTO> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	};
+}
--- backend/src/main/java/com/tadeifelipe/dsdeliver/repositories/OrderRepository.java
+++ backend/src/main/java/com/tadeifelipe/dsdeliver/repositories/OrderRepository.java
@@ -1,9 +1,15 @@
 package com.tadeifelipe.dsdeliver.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.tadeifelipe.dsdeliver.entities.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long>  {
 	
+	@Query("select distinct obj from Order obj join fetch obj.products"
+		 + " where obj.status = 0 order by obj.moment asc")
+	List<Order> findOrdersWithProducts();
 }
